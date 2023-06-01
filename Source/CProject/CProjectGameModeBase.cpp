76e6a89574c79c94177df92803d4b6c46bd9ae12
Jay9011 <63570839+Jay9011@users.noreply.github.com>
2023-06-02 03:09:36 +0900

    Init

---
+++ Source/CProject/CProjectGameModeBase.cpp
@@ -0,0 +1,2 @@
+#include "CProjectGameModeBase.h"
+
---
+++ Source/CProject/CProjectGameModeBase.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CProjectGameModeBase.generated.h"
+
+UCLASS()
+class CPROJECT_API ACProjectGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
